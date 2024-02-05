b50c22df705eb71cac86302fd74d647193421ee7
Tristen Haynes <tristen.haynes@fbitn.com>
2024-02-05 12:48:12 -0600

    basic layout and install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { ModeToggle } from "@/components/ui/toggle-theme";
+
 export default function Home() {
   return (
     <main>
+      <ModeToggle />
       <h1>Book Assistant</h1>
       <p>
         Utilizes the Google Books API to search for books and display details
